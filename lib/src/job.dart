ca67d6438f9ba66ad672aec90af4e24bcb590f2a
Cédric Belin <cedric@belin.io>
2017-01-29 02:34:15 +0100

    Code factorization

--- lib/src/job.dart
+++ lib/src/job.dart
@@ -4,49 +4,69 @@ part of coveralls;
 class Job {
 
   /// Creates a new job from the specified list of source files.
-  Job([List<SourceFile> sourceFiles]): sourceFiles = sourceFiles ?? [];
+  Job([Configuration config, List<SourceFile> sourceFiles]): sourceFiles = sourceFiles ?? [] {
+    if (config != null) {
+      var hasGitData = config.keys.any((key) => key == 'service_branch' || key.substring(0, 4) == 'git_');
+      if (!hasGitData) this.commitSha = config['commit_sha'] ?? '';
+      else {
+        var commit = new GitCommit(config['commit_sha'] ?? '', config['git_message'] ?? '');
+        commit.authorEmail = config['git_author_email'] ?? '';
+        commit.authorName = config['git_author_name'] ?? '';
+        commit.committerEmail = config['git_committer_email'] ?? '';
+        commit.committerName = config['git_committer_email'] ?? '';
+
+        this.git = new GitData(commit, config['service_branch'] ?? '');
+      }
+
+      isParallel = config['parallel'] == 'true';
+      repoToken = config['repo_token'] ?? (config['repo_secret_token'] ?? '');
+      runAt = config['run_at'] != null ? DateTime.parse(config['run_at']) : null;
+      serviceJobId = config['service_job_id'] ?? '';
+      serviceName = config['service_name'] ?? '';
+      serviceNumber = config['service_number'] ?? '';
+      servicePullRequest = config['service_pull_request'] ?? '';
+    }
+  }
 
   /// Creates a new job from the specified [map] in JSON format.
   Job.fromJson(Map<String, dynamic> map):
-    commitSha = map['commit_sha'] != null ? map['commit_sha'].toString() : null,
-    git = map['git'] is Map<String, dynamic> ? map['git'] : null,
-    parallel = map['parallel'] is bool ? map['parallel'] : null,
-    //repoToken = map['repo_token'] != null ? map['repo_token'].toString() : null,
-    runAt = map['run_at'] != null ? DateTime.parse(map['run_at'].toString()) : null,
-    serviceJobId = map['service_job_id'] != null ? map['service_job_id'].toString() : null,
-    //serviceName = map['service_name'] != null ? map['service_name'].toString() : null,
-    serviceNumber = map['service_number'] != null ? map['service_number'].toString() : null,
-    servicePullRequest = map['service_pull_request'] != null ? map['service_pull_request'].toString() : null,
+    commitSha = map['commit_sha'] is String ? map['commit_sha'] : null,
+    git = map['git'] is Map<String, dynamic> ? new GitData.fromJson(map['git']) : null,
+    isParallel = map['parallel'] is bool ? map['parallel'] : false,
+    repoToken = map['repo_token'] is String ? map['repo_token'] : '',
+    runAt = map['run_at'] is String ? DateTime.parse(map['run_at']) : null,
+    serviceJobId = map['service_job_id'] is String ? map['service_job_id'] : '',
+    serviceName = map['service_name'] is String ? map['service_name'] : '',
+    serviceNumber = map['service_number'] is String ? map['service_number'] : '',
+    servicePullRequest = map['service_pull_request'] is String ? map['service_pull_request'] : '',
     sourceFiles = map['source_files'] is List<Map> ? map['source_files'].map((item) => new SourceFile.fromJson(item)).toList() : [];
 
   /// The current SHA of the commit being built to override the [git] property.
-  String commitSha;
+  String commitSha = '';
 
   /// A map of Git data that can be used to display more information to users.
-  Map<String, dynamic> git;
+  GitData git;
 
   /// Value indicating whether the build will not be considered done until a webhook has been sent to Coveralls.
-  bool parallel;
+  bool isParallel = false;
 
   /// The secret token for the repository.
-  String get repoToken => ''; //config['repo_token'];
-  set repoToken(String value) => null; // config['repo_token'] = value;
+  String repoToken = '';
 
   /// A timestamp of when the job ran.
   DateTime runAt;
 
-  /// A unique identifier of the job on the service specified by [serviceName].
-  String serviceJobId;
+  /// A unique identifier of the job on the CI service.
+  String serviceJobId = '';
 
   /// The CI service or other environment in which the test suite was run.
-  String get serviceName => ''; //config['service_name'];
-  set serviceName(String value) => null; //config['service_name'] = value;
+  String serviceName = '';
 
   /// The build number.
-  String serviceNumber;
+  String serviceNumber = '';
 
   /// The associated pull request ID of the build.
-  String servicePullRequest;
+  String servicePullRequest = '';
 
   /// The list of source files.
   final List<SourceFile> sourceFiles;
@@ -55,16 +75,16 @@ class Job {
   Map<String, dynamic> toJson() {
     var map = <String, dynamic>{};
 
-    if (repoToken != null) map['repo_token'] = repoToken;
-    if (serviceName != null) map['service_name'] = serviceName;
-    if (serviceNumber != null) map['service_number'] = serviceNumber;
-    if (serviceJobId != null) map['service_job_id'] = serviceJobId;
-    if (servicePullRequest != null) map['service_pull_request'] = servicePullRequest;
+    if (repoToken.isNotEmpty) map['repo_token'] = repoToken;
+    if (serviceName.isNotEmpty) map['service_name'] = serviceName;
+    if (serviceNumber.isNotEmpty) map['service_number'] = serviceNumber;
+    if (serviceJobId.isNotEmpty) map['service_job_id'] = serviceJobId;
+    if (servicePullRequest.isNotEmpty) map['service_pull_request'] = servicePullRequest;
 
     map['source_files'] = sourceFiles.map((item) => item.toJson()).toList();
-    if (parallel != null) map['parallel'] = parallel;
-    if (git != null) map['git'] = git;
-    if (commitSha != null) map['commit_sha'] = commitSha;
+    if (isParallel) map['parallel'] = true;
+    if (git != null) map['git'] = git.toJson();
+    if (commitSha.isNotEmpty) map['commit_sha'] = commitSha;
     if (runAt != null) map['run_at'] = runAt.toIso8601String();
 
     return map;
