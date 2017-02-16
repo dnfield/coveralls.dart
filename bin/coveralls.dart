4b31dc916e48efcb44d1bf9215ce0ced1930a1d1
Cédric Belin <cedric@belin.io>
2017-02-16 13:51:22 +0100

    Implemented the CLI script

--- bin/coveralls.dart
+++ bin/coveralls.dart
@@ -1,25 +1,41 @@
 #!/usr/bin/env dart
 
+import 'dart:async';
 import 'dart:io';
 import 'package:args/args.dart';
 import 'package:coveralls/coveralls.dart';
 
 /// The command line argument parser.
 final ArgParser _parser = new ArgParser()
+  ..addOption('file', abbr: 'f', help: 'path to the coverage report', valueHelp: 'file')
   ..addFlag('help', abbr: 'h', help: 'output usage information', negatable: false)
   ..addFlag('version', abbr: 'v', help: 'output the version number', negatable: false);
 
 /// Application entry point.
-void main(List<String> arguments) {
+Future main(List<String> arguments) async {
   try {
     var results = _parser.parse(arguments);
     if (results['help']) return printUsage();
-    if (results['version']) return print('v$version');
+    if (results['version']) return print(version);
+
+    if (results['file'] == null) {
+      printUsage();
+      exit(1);
+    }
+
+    var file = new File(results['file']);
+    if (!await file.exists()) throw new FileSystemException('The specified file is not found.', file.path);
+
+    var client = new Client(Platform.environment['COVERALLS_ENDPOINT'] ?? Client.defaultEndPoint);
+    var coverage = await file.readAsString();
+    
+    print('[Coveralls] Submitting to ${client.endPoint}');
+    await client.upload(coverage);
   }
 
-  on ArgParserException catch(error) {
-    print(error.message);
-    exit(1);
+  catch(error) {
+    print(error);
+    exit(2);
   }
 }
 
