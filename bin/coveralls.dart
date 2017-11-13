a5e68fa39788707108a528b7dca6116cb0866097
Cédric Belin <cedric@belin.io>
2017-11-13 01:47:27 +0100

    Bumped the version number to v6.0.0 to match the next NPM release

--- bin/coveralls.dart
+++ bin/coveralls.dart
@@ -6,7 +6,7 @@ import 'package:args/args.dart';
 import 'package:coveralls/coveralls.dart';
 
 /// The version number of this package.
-const String version = '2.1.0';
+const String version = '6.0.0';
 
 /// The command line argument parser.
 final ArgParser _parser = new ArgParser()
