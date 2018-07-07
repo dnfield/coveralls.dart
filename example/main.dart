68a4d9c88123527a843dae0b554b0e96b767efbf
Cédric Belin <cedric@belin.io>
2018-07-08 00:59:59 +0200

    Added an example code

---
+++ example/main.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:coveralls/coveralls.dart';
+
+Future<void> main() async {
+  try {
+    var coverage = File('/path/to/coverage.report');
+    await Client().upload(await coverage.readAsString());
+    print('The report was sent successfully.');
+  }
+
+  on Exception catch (err) {
+    print('An error occurred: $err');
+  }
+}
