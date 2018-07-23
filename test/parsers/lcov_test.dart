0a4dc1ad98089c30d7494c67989c236edaba7eb3
Cédric Belin <cedric@belin.io>
2018-07-23 19:49:48 +0200

    Fixed the unit tests

--- test/parsers/lcov_test.dart
+++ test/parsers/lcov_test.dart
@@ -10,15 +10,15 @@ void main() => group('Lcov', () {
       var job = await parseReport(await File('test/fixtures/lcov.info').readAsString());
       expect(job.sourceFiles, hasLength(3));
 
-      expect(job.sourceFiles.first.name, equals(p.join('lib', 'src', 'client.dart')));
+      expect(job.sourceFiles.first.name, equals(p.join('lib', 'src', 'io', 'client.dart')));
       expect(job.sourceFiles.first.sourceDigest, isNotEmpty);
       expect(job.sourceFiles.first.coverage, containsAllInOrder([null, 2, 2, 2, 2, null]));
 
-      expect(job.sourceFiles[1].name, equals(p.join('lib', 'src', 'configuration.dart')));
+      expect(job.sourceFiles[1].name, equals(p.join('lib', 'src', 'io', 'configuration.dart')));
       expect(job.sourceFiles[1].sourceDigest, isNotEmpty);
       expect(job.sourceFiles[1].coverage, containsAllInOrder([null, 4, 4, 2, 2, 4, 2, 2, 4, 4, null]));
 
-      expect(job.sourceFiles[2].name, equals(p.join('lib', 'src', 'git.dart')));
+      expect(job.sourceFiles[2].name, equals(p.join('lib', 'src', 'io', 'git.dart')));
       expect(job.sourceFiles[2].sourceDigest, isNotEmpty);
       expect(job.sourceFiles[2].coverage, containsAllInOrder([null, 2, 2, 2, 2, 2, 0, 0, 2, 2, null]));
     });
