0405d564462d2e7764e401eab61122afb1a80c4b
Cédric Belin <cedric@belin.io>
2018-08-12 02:19:18 +0200

    Fixed a parsing issue

--- lib/src/io/services/circleci.dart
+++ lib/src/io/services/circleci.dart
@@ -3,7 +3,7 @@ import 'package:coveralls/coveralls.dart';
 /// Gets the configuration parameters from the specified environment.
 Configuration getConfiguration(Map<String, String> env) => Configuration({
   'commit_sha': env['CIRCLE_SHA1'],
-  'parallel': int.tryParse(env['CIRCLE_NODE_TOTAL'] ?? '0', radix: 1) ?? 0 > 1 ? 'true' : 'false',
+  'parallel': int.tryParse(env['CIRCLE_NODE_TOTAL'] ?? '0', radix: 10) ?? 0 > 1 ? 'true' : 'false',
   'service_branch': env['CIRCLE_BRANCH'],
   'service_build_url': env['CIRCLE_BUILD_URL'],
   'service_job_number': env['CIRCLE_NODE_INDEX'],
